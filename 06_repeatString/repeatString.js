ee6217015a89fbcbb4f7661e3deef587275e86aa
101010watson <abhisheksahu22999@gmail.com>
2025-08-19 14:03:43 +0530

    completed the repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let rep = "";
+    if(num < 0) return 'ERROR';
+    for(let i = 0;i<num;i++){
+        rep+=string;
+    }
+    return rep;
 };
 
 // Do not edit below this line
