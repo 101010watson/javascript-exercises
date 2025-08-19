c9a75ee4082060d22c859858c0f45e27372cd4a9
101010watson <abhisheksahu22999@gmail.com>
2025-08-19 14:24:15 +0530

    done with reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    if(string === '') return '';
+    let reversed = '';
+    for(let i = string.length; i >= 0;i--){
+        reversed += string.slice(i,i+1);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
