df6084d1e926b3cb0280997e6f8c09069ddc5b2f
101010watson <abhisheksahu22999@gmail.com>
2025-08-20 00:08:36 +0530

    done some exercises

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    return arr.filter(x => !args.includes(x));
+    // Explanation - so the x iterator iterates through the array and checks 
+    // if the argument is present (expression become false) do not include that element in the array(arr)
+    // in the array or not (expression becomes true) include the element in the array(arr)
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return 'ERROR';
+    if(min<0 || max < 0) return 'ERROR';
+    let res = 0;
+    if(min>max) [min,max] = [max,min];
+    for(let i = min;i<=max;i++){
+        res+=i;
+    }
+    return res;
 };
 
 // Do not edit below this line
